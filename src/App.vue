51ac477615c90280d526bda020ac857d906c2340
FloxOoO <106467341+FloxOoO@users.noreply.github.com>
2022-10-10 08:56:04 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <button @click="isOpen = true">Открыть</button>
+  <Modal :is-open="isOpen" @close="isOpen = false" @ok="modalOK">
+    Данил бариев гей?
+    <template #actions="{ confirm }">
+      <input :placeholder="$options.MODAL_TEXT" v-model="confirmation" />
+      <button :disabled="!confirmed" @click="confirm">OK</button>
+    </template>
+  </Modal>
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+export default {
+  name: "App",
+  components: {
+    Modal,
+  },
+  MODAL_TEXT: "ПОДТВЕРЖДАЮ",
+  data() {
+    return { isOpen: false, confirmation: "" };
+  },
+  methods: {
+    modalOK() {
+      alert("Вы правы!!!");
+    },
+  },
+  computed: {
+    confirmed() {
+      return this.confirmation === this.$options.MODAL_TEXT;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
